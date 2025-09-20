787748217328e5e3a25b8313e8ad3016b1d2a681
Ricardo Leidenz <139507934+RicardoLeidenz@users.noreply.github.com>
2025-09-20 02:41:29 +0000

    Finished adding User, Planet, Vehicle and Character tables and initial operations

--- src/app.py
+++ src/app.py
@@ -8,15 +8,16 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Planet, Character, Vehicle
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,23 +28,90 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+### USER OPERATIONS ###
 @app.route('/user', methods=['GET'])
-def handle_hello():
-
+def handle_users():
+    users = User.query.all()
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "users": [item.serialize() for item in users]
     }
-
     return jsonify(response_body), 200
+
+
+### PLANET OPERATIONS ###
+@app.route('/planet', methods=['GET'])
+def handle_planets():
+    planets = Planet.query.all()
+    response_body = {
+        "planets": [item.serialize() for item in planets]
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/planet/favorite', methods=['POST'])
+def fav_planet():
+    data = request.get_json()
+    user_id = data["user"]
+    planet_id = data["planet"]
+    user = db.session.get(User, user_id)
+    planet = db.session.get(Planet, planet_id)
+    user.favorite_planets.append(planet)
+    db.session.commit()
+
+    return jsonify({"user": user.serialize()}), 200
+
+### CHARACTER OPERATIONS ###
+@app.route('/character', methods=['GET'])
+def handle_characters():
+    characters = Character.query.all()
+    response_body = {
+        "characters": [item.serialize() for item in characters]
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/character/favorite', methods=['POST'])
+def fav_character():
+    data = request.get_json()
+    user_id = data["user"]
+    character_id = data["character"]
+    user = db.session.get(User, user_id)
+    character = db.session.get(Character, character_id)
+    user.favorite_characters.append(character)
+    db.session.commit()
+
+    return jsonify({"user": user.serialize()}), 200
+
+### VEHICLE OPERATIONS ###
+@app.route('/vehicle', methods=['GET'])
+def handle_vehicles():
+    vehicles = Vehicle.query.all()
+    response_body = {
+        "vehicles": [item.serialize() for item in vehicles]
+    }
+    return jsonify(response_body), 200
+
+@app.route('/user/vehicle/favorite', methods=['POST'])
+def fav_vehicle():
+    data = request.get_json()
+    user_id = data["user"]
+    vehicle_id = data["vehicle"]
+    user = db.session.get(User, user_id)
+    vehicle = db.session.get(Vehicle, vehicle_id)
+    user.favorite_vehicles.append(vehicle)
+    db.session.commit()
+
+    return jsonify({"user": user.serialize()}), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,77 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, Column, ForeignKey, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+favorite_planet = Table(
+    "favorite_planet",
+    db.Model.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("planet_id", ForeignKey("planet.id"), primary_key=True)
+)
+favorite_character = Table(
+    "favorite_character",
+    db.Model.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("character_id", ForeignKey("character.id"), primary_key=True)
+)
+favorite_vehicle = Table(
+    "favorite_vehicle",
+    db.Model.metadata,
+    Column("user_id", Integer, ForeignKey("user.id"), primary_key=True),
+    Column("vehicle_id", ForeignKey("vehicle.id"), primary_key=True)
+)
+
 
 class User(db.Model):
+    __tablename__ = "user"
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
+    email = Column(String(120), unique=True, nullable=False)
+    favorite_planets = relationship("Planet", secondary=favorite_planet)
+    favorite_characters = relationship("Character", secondary=favorite_character)
+    favorite_vehicles = relationship("Vehicle", secondary=favorite_vehicle)
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "favorite_planets": [item.serialize() for item in self.favorite_planets],
+            "favorite_characters": [item.serialize() for item in self.favorite_characters],
+            "favorite_vehicle": [item.serialize() for item in self.favorite_vehicle]
+        }
+
+
+class Planet(db.Model):
+    __tablename__ = "planet"
+    id = Column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Character(db.Model):
+    __tablename__ = "character"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+
+class Vehicle(db.Model):
+    __tablename__ = "vehicle"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
         }
